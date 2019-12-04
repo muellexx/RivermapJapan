dbed24a3079152a2c375c3693530cbd933edd825
muellexx <alex.muellex@gmail.com>
2019-12-05 01:08:24 +0900

    Managed static files

--- static/js/map.js
+++ static/js/map.js
@@ -1,3 +1,16 @@
+var rivers = $.getJson( "map.json", function() {
+  console.log( "success" );
+})
+  .done(function() {
+    console.log( "second success" );
+  })
+  .fail(function() {
+    console.log( "error" );
+  })
+  .always(function() {
+    console.log( "complete" );
+  });
+
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
@@ -78,6 +91,50 @@ function initMap() {
         ]
     });
 
-    var river = [];
-    var infowindow = [];
+    //var river = [];
+    //var infowindow = [];
+
+    var id = 0;
+    var riverCoordinates = [{
+            lat: 35.8,
+            lng: 139.176
+        },
+        {
+            lat: 35.785,
+            lng: 139.257
+        }
+    ];
+
+    var river = new google.maps.Polyline({
+        path: riverCoordinates,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 5
+    });
+
+    var contentString = '<div id="content">' +
+        '<div id="siteNotice">' +
+        '</div>' +
+        '<h1 id="firstHeading" class="firstHeading">Tama River</h1>' +
+        '<div id="bodyContent">' +
+        '<p><b>Tama River</b>, located in <b>Tokyo Prefecture</b>, is a local ' +
+        'River of WhiteWater Level II-III.</p>' +
+        '</div>' +
+        '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString,
+    });
+
+    river.addListener('mouseover', function(event) {
+        infowindow.setPosition(event.latLng);
+        infowindow.open(map);
+    });
+
+    river.addListener('mouseout', function() {
+        infowindow.close(map);
+    });
+
+    river.setMap(map);
 }
