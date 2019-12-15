d456b190216410a0b2bcde2a840508b179581778
muellexx <alex.muellex@gmail.com>
2019-12-15 20:25:40 +0900

    Adjusted Style for popup/popdown menu of river

--- static/js/init-map.js
+++ static/js/init-map.js
@@ -1,8 +1,8 @@
-var map;
+var map, popup, Popup;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
+        zoom: 8,
         center: {
             lat: 35.802514,
             lng: 139.194369
@@ -79,5 +79,122 @@ function initMap() {
             }
         ]
     });
-    loadRivers();
+    Popup = createPopupClass();
+    popup = new Popup(
+        new google.maps.LatLng(35.8, 139.19),
+        document.getElementById('content'));
+    popup.setMap(map);
+    loadRivers(popup);
+}
+
+/**
+ * Returns the Popup class.
+ *
+ * Unfortunately, the Popup class can only be defined after
+ * google.maps.OverlayView is defined, when the Maps API is loaded.
+ * This function should be called by initMap.
+ */
+function createPopupClass() {
+  /**
+   * A customized popup on the map.
+   * @param {!google.maps.LatLng} position
+   * @param {!Element} content The bubble div.
+   * @constructor
+   * @extends {google.maps.OverlayView}
+   */
+  function Popup(position, content) {
+    this.position = position;
+
+    content.classList.add('popup-bubble');
+
+    // This zero-height div is positioned at the bottom of the bubble.
+    var bubbleAnchor = document.createElement('div');
+    bubbleAnchor.classList.add('popup-bubble-anchor');
+    bubbleAnchor.appendChild(content);
+
+    // This zero-height div is positioned at the bottom of the tip.
+    this.containerDiv = document.createElement('div');
+    this.containerDiv.classList.add('popup-container');
+    this.containerDiv.appendChild(bubbleAnchor);
+    this.containerDiv.style.visibility = 'hidden';
+
+    // Optionally stop clicks, etc., from bubbling up to the map.
+    google.maps.OverlayView.preventMapHitsAndGesturesFrom(this.containerDiv);
+  }
+  // ES5 magic to extend google.maps.OverlayView.
+  Popup.prototype = Object.create(google.maps.OverlayView.prototype);
+
+  /** Called when the popup is added to the map. */
+  Popup.prototype.onAdd = function() {
+    this.getPanes().floatPane.appendChild(this.containerDiv);
+  };
+
+  /** Called when the popup is removed from the map. */
+  Popup.prototype.onRemove = function() {
+    if (this.containerDiv.parentElement) {
+      this.containerDiv.parentElement.removeChild(this.containerDiv);
+    }
+  };
+
+  /** Called each frame when the popup needs to draw itself. */
+  Popup.prototype.draw = function() {
+    var divPosition = this.getProjection().fromLatLngToDivPixel(this.position);
+    //content.innerHTML = divPosition.y + ", ";
+    //content.innerHTML = this.containerDiv.children[0].className; //divPosition.y + ", ";
+    if(divPosition.y > 0){
+        content.className = "popup-bubble";
+        this.containerDiv.children[0].className = "popup-bubble-anchor";
+        this.containerDiv.className = "popup-container";
+    }else{
+        content.className = "popdown-bubble";
+        this.containerDiv.children[0].className = "popdown-bubble-anchor";
+        this.containerDiv.className = "popdown-container";
+        //bubbleAnchor.className = "popdown-bubble-anchor";
+        //this.containerDiv.children[0].style.border-left = 12px;
+    }
+
+    // Hide the popup when it is far out of view.
+    var display =
+        Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000 ?
+        'block' :
+        'none';
+
+    if (display === 'block') {
+      this.containerDiv.style.left = divPosition.x + 'px';
+      this.containerDiv.style.top = divPosition.y + 'px';
+    }
+
+    // Set the visibility to 'hidden' or 'visible'.
+    Popup.prototype.hide = function() {
+      if (this.containerDiv) {
+        // The visibility property must be a string enclosed in quotes.
+        this.containerDiv.style.visibility = 'hidden';
+      }
+    };
+
+    Popup.prototype.show = function() {
+    if (this.containerDiv) {
+        this.containerDiv.style.visibility = 'visible';
+      }
+    };
+
+    if (this.containerDiv.style.display !== display) {
+      this.containerDiv.style.display = display;
+    }
+
+    Popup.prototype.setPosition = function(position) {
+        this.position = position;
+    }
+
+    Popup.prototype.setContent = function(river) {
+        //console.log(river);
+        content.innerHTML = "<h3>" + river.name + "</h3>";
+        content.innerHTML += "<p><b>Current Level: " + river.level + " m</b></br>";
+        content.innerHTML += "Updated: " + river.updated + "</p>";
+        content.innerHTML += "LW: " + river.lowwater + " &nbsp; MW: " + river.middlewater + " &nbsp; HW: " + river.highwater;
+        content.innerHTML += '<p align="right"><a href="' + river.url + '" target="_blank">Source</a></p>';
+    }
+  };
+
+  return Popup;
 }
--- static/js/map.js
+++ static/js/map.js
@@ -1,27 +1,8 @@
-/*var rivrs;
-$.getJSON("static/js/river.json", function(json) {
-        rivrs = json;
-        console.log("success");
-    })
-    .done(function() {
-        console.log("second success");
-    })
-    .fail(function() {
-        console.log("error");
-    })
-    .always(function() {
-        console.log("complete");
-    });*/
-
-function loadRivers() {
-    $.getJSON("static/js/river.json", setRivers);
-    //console.log("hi");
-}
-
-function setRivers(json) {
-    rivers = json.rivers;
-    for (let i = 0; i < rivers.length; i++) {
-        river = rivers[i];
+function loadRivers(popup) {
+    $.getJSON("static/js/river.json", function(json){
+        rivers = json.rivers;
+        for (let i = 0; i < rivers.length; i++) {
+        var iriver = rivers[i];
         var riverCoordinates = [{
                 lat: rivers[i].start_lat,
                 lng: rivers[i].start_lng
@@ -33,47 +14,37 @@ function setRivers(json) {
         ];
 
         var river_color;
-        if (river.level >= river.highwater){
+        if (iriver.level >= iriver.highwater){
             river_color = '#FF0000';
-        } else if (river.level >= river.middlewater){
+        } else if (iriver.level >= iriver.middlewater){
             river_color = '#00FF00';
-        } else if (river.level >= river.lowwater){
+        } else if (iriver.level >= iriver.lowwater){
             river_color = '#0000FF';
         } else {
             river_color = '#999999';
         }
-        console.log(river_color);
+
         var river = new google.maps.Polyline({
             path: riverCoordinates,
             geodesic: true,
             strokeColor: river_color,
             strokeOpacity: 1.0,
-            strokeWeight: 5
-        });
-
-        var contentString = '<div id="content">' +
-            '<div id="siteNotice">' +
-            '</div>' +
-            '<h1 id="firstHeading" class="firstHeading">' + rivers[i].name + '</h1>' +
-            '<div id="bodyContent">' +
-            '<p><b>Waterevel:</b>' + rivers[i].level + '</p>' +
-            '<p><a href="' + rivers[i].url + '" target="_blank">Source</a></p>' +
-            '</div>' +
-            '</div>';
-
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString,
+            strokeWeight: 5,
+            id: i
         });
 
         river.addListener('mouseover', function(event) {
-            infowindow.setPosition(event.latLng);
-            infowindow.open(map);
+            popup.setContent(rivers[i]);
+            popup.show();
+            popup.setPosition(event.latLng);
+            popup.draw();
         });
 
         river.addListener('mouseout', function() {
-            infowindow.close(map);
+            popup.hide();
         });
 
         river.setMap(map);
     }
+    });
 }
