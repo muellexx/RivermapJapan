d16f4b9050b1a4cdb86e893b2765f5a266dce21e
muellexx <alex.muellex@gmail.com>
2019-12-23 00:38:45 +0900

    Added function to change profile information and picture

--- users/models.py
+++ users/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
+
+
+def crop_center(pil_img, crop_width, crop_height):
+    img_width, img_height = pil_img.size
+    return pil_img.crop(((img_width - crop_width) // 2,
+                         (img_height - crop_height) // 2,
+                         (img_width + crop_width) // 2,
+                         (img_height + crop_height) // 2))
 
 
 class Profile(models.Model):
@@ -8,4 +17,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+        img = crop_center(img, min(img.size), min(img.size))
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
