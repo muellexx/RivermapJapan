e42e6bd3d9d388eb48d04a110cb7ec64cd52bb7c
muellexx <alex.muellex@gmail.com>
2019-12-31 16:23:54 +0900

    Started coding function to add rivers

--- rivermap/urls.py
+++ rivermap/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
-
+from .views import RiverCreateView, RiverDetailView
 
 urlpatterns = [
-    path('', views.rivermap, name="rivermap")
+    path('', views.rivermap, name="rivermap"),
+    path('river/<int:pk>/', RiverDetailView.as_view(), name='river-detail'),
+    path('river/new/', RiverCreateView.as_view(), name='river-create'),
 ]
--- rivermap/views.py
+++ rivermap/views.py
@@ -1,7 +1,25 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views.generic import CreateView, DetailView
+
 from .models import River
 
 
 def rivermap(request):
     rivers = River.objects.all()
     return render(request, 'rivermap/map.html', {'title': 'Map', 'rivers': rivers})
+
+
+class RiverDetailView(DetailView):
+    model = River
+
+
+class RiverCreateView(LoginRequiredMixin, CreateView):
+    model = River
+    fields = ['name', 'url', 'level', 'date', 'high_water', 'middle_water', 'low_water', 'start_lat', 'start_lng',
+              'end_lat', 'end_lng', 'difficulty', 'section']
+    success_url = '/'
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
